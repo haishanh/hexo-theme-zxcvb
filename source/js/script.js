658722ba7bd6e085c0296e2e301e33840a02634c
haishanh <haishanhan@gmail.com>
2016-02-16 18:44:32 +0800

    Completely rewrite the post nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,38 @@
-(function (d){
+(function (d, w){
   // LESS TYPING
   var $ = d.querySelector.bind(d);
+  var $$ = d.querySelectorAll.bind(d);
 
-  // DISQUS
-  var disqus = $('#disqus_thread');
-  if (disqus) {
-    var commentButton = $('#show-comment');
-    if (commentButton) {
-      commentButton.addEventListener('click', function (ev) {
-        ev.preventDefault();
-        disqus.style.display = disqus.style.display === 'block'
-          ? 'none'
-          : 'block';
-      })
+  // POSTS NAV POSITION
+  var nav = $('.post-nav');
+  var navTop = nav.getBoundingClientRect().top;
+  var navUpdateTicking = false;
+
+  if (nav) {
+    w.addEventListener('scroll', function (e) {
+      navTop = nav.getBoundingClientRect().top
+      requestTick();
+    });
+  }
+
+  function requestTick() {
+    if(!navUpdateTicking) {
+      requestAnimationFrame(updateNav);
+    }
+    navUpdateTicking = true;
+  }
+
+  function updateNav() {
+    navUpdateTicking = false;
+
+    var prev = $('.post-nav .prev');
+    var next = $('.post-nav .next');
+    if (navTop < w.innerHeight) {
+      prev && !prev.classList.contains('stop') && prev.classList.add('stop');
+      next && !next.classList.contains('stop') && next.classList.add('stop');
+    } else {
+      prev && prev.classList.contains('stop') && prev.classList.remove('stop');
+      next && next.classList.contains('stop') && next.classList.remove('stop');
     }
   }
-}(document))
+}(document, window))
